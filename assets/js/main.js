98998c8ffc2ca9554c47b2178ca1b91fe4627036
daniel-s-souza <danielsantsouza94@gmail.com>
2022-10-26 17:31:25 -0300

    feat: faz primeira requisição na API pokeAPI

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+const offset = 0;
+const limit = 10;
+
+const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(URL).then((response) => {
+  console.log(response);
+});
