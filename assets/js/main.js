81e5d9bbf0a19364e81c3cc3b0f953f8f0cd6943
daniel-s-souza <danielsantsouza94@gmail.com>
2022-10-27 17:06:04 -0300

    feat: implementa css dinamico com classe de types dos pokemons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 
 function convertPokemonHtml (pokemon) {
   return `
-  <li class="Pokemon">
+  <li class="Pokemon ${pokemon.type}">
   <span class="PokeNumber"> #00${pokemon.number} </span>
   <span class="Name">${pokemon.name}</span>
   <div class="detail">
     <ol class="Types">
-        ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
     </ol>
     <img class="pokemonImg" src="${pokemon.image}" alt=${pokemon.name}>
   </div>
