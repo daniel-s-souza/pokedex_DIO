dc1467058cb46c05270f48643a3b4dcc43c1b6e3
daniel-s-souza <danielsantsouza94@gmail.com>
2022-10-27 14:12:42 -0300

    feat: implementa catch e finaly na requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,8 @@ const URL = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 fetch(URL).then((response) => {
   console.log(response);
+}).catch((error) => {
+  console.log(error);
+}).finally(() => {
+  console.log("Done");
 });
